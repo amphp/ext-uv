ed029a78e3e65e89f04216ec440798e35d3147a1
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2012-05-30 01:51:31 +0900

    add udp example

---
+++ examples/udp_bind.php
@@ -0,0 +1,19 @@
+<?php
+$udp = uv_udp_init();
+var_dump($udp);
+
+uv_udp_bind($udp, '0.0.0.0',10000);
+
+uv_udp_recv_start($udp,function($buffer,$udp){
+    echo "recv:" .  $buffer;
+    
+    uv_close($udp);
+});
+
+$uv = uv_udp_init();
+uv_udp_send($uv, "Hello", uv_ip4_addr("0.0.0.0",10000),function($s,$uv){
+    echo "success" . PHP_EOL;
+    uv_close($uv);
+});
+
+uv_run();
