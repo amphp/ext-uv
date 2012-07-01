db85553439d8e7bd0184a17b10203f8cc6f7d4a6
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2012-07-01 21:58:12 +0900

    add http server sample

---
+++ examples/debug_timer.php
@@ -0,0 +1,10 @@
+<?php
+$timer = uv_timer_init();
+
+$stat = array();
+$stat['begin'] = memory_get_usage();
+
+uv_timer_start($timer, 10, 1000, function($stat, $timer) use (&$stat){
+    $stat["current"] = memory_get_usage();
+    printf("memory: %d\n", $stat["current"] - $stat['begin']);
+});
