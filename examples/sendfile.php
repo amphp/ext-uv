515e4071fa70ce983bb75712ab3c9491b4d4148b
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2012-07-15 00:44:59 +0900

    add uv_fs_sendfile test case.
    TODO: obtain file handle from php_uv resource.

--- examples/sendfile.php
+++ examples/sendfile.php
@@ -1,11 +1,8 @@
 <?php
 
-uv_fs_open(uv_default_loop(),__FILE__,UV::O_RDONLY,0, function($r){
-    uv_fs_open(uv_default_loop(),"moe.out",UV::O_WRONLY | UV::O_CREAT ,0644, function($x) use ($r){
-        uv_fs_sendfile(uv_default_loop(),$r,$x,0,126, function($result){
-            echo "sendfile";
-            var_dump($result);
-    });
+uv_fs_open(uv_default_loop(),__FILE__,UV::O_RDONLY,0, function($read_fd){
+    $stdout = 0;
+    uv_fs_sendfile(uv_default_loop(),$stdout,$read_fd,0,6, function($result){
     });
 });
 
---
+++ tests/320-fs-sendfile.phpt
@@ -0,0 +1,15 @@
+--TEST--
+Check for fs_send_file
+--FILE--
+<?php
+define("FIXTURE_PATH", dirname(__FILE__) . "/fixtures/hello.data");
+
+uv_fs_open(uv_default_loop(), FIXTURE_PATH, UV::O_RDONLY, 0, function($read_fd){
+    $std_err = 1; // phpt doesn't catch stdout as uv_fs_sendfile uses stdout directly.
+    uv_fs_sendfile(uv_default_loop(),$std_err, $read_fd, 0, 6, function($result){
+    });
+});
+
+uv_run();
+--EXPECT--
+Hello
