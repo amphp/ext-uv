32ea5e154f1ccb1b76fc7c526d4bbb5a830e23f3
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2012-07-16 00:10:55 +0900

    adjust poll example [ci skip]

--- examples/poll.php
+++ examples/poll.php
@@ -1,21 +1,25 @@
 <?php
 
 $socket = stream_socket_server("tcp://0.0.0.0:9999", $errno, $errstr);
+stream_set_blocking($socket, 0);
 
 $poll = uv_poll_init(uv_default_loop(), $socket);
+
 uv_poll_start($poll, UV::READABLE, function($poll, $stat, $ev, $socket){
+    echo "parent poll:\n";
+    var_dump($stat);
     $conn = stream_socket_accept($socket);
-    echo "poll";
+
     $pp = uv_poll_init(uv_default_loop(), $conn);
-    uv_poll_start($pp, UV::READABLE | UV::WRITABLE, function($poll, $stat, $ev, $conn){
-        echo "cb";
-        
-        uv_fs_write(uv_default_loop(), $conn, "echo", -1, function($fs, $conn){
-            var_dump($fs);
-        });
-        
-        uv_fs_close(uv_default_loop(), $conn, function(){
-            echo "close";
+    uv_poll_start($pp, UV::WRITABLE, function($poll, $stat, $ev, $conn){
+        echo "  cb";
+        var_dump($stat);
+        var_dump($conn);
+
+        uv_poll_stop($poll);
+        uv_fs_write(uv_default_loop(), $conn, "Hello World", -1, function($conn, $nwrite){
+            var_dump($conn);
+            fclose($conn);
         });
     });
 });
