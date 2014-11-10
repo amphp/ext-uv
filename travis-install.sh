9d6e66d5160c6980341e35ef3dbe8c40cd7200c8
Steve Rhoades <sedonami@gmail.com>
2014-11-10 15:46:49 +0000

    add test for fs_close issue described in #36, updating travis build

---
+++ travis-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+# install 'libuv'
+git clone --recursive --branch v1.0.0-rc2 --depth 1 https://github.com/joyent/libuv.git
+pushd libuv
+./autogen.sh && ./configure && make && sudo make install
+popd
+
+#install 'php-uv'
+phpize && ./configure --with-uv --enable-httpparser && make && sudo make install
+echo "extension=uv.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
