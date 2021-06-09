24f2a44963adc3123c206814c292ba5e11b94f96
L. Stubbs <lstubbs@techno.express>
2021-06-08 20:55:25 -0400

    fatal error fix, allows extension to a least compile on PHP 8
    - resolves `error C2106: '=': left operand must be l-value` line 4890 and 4891 mainly.
    - not sure of previous tests, but currently have 4 fails, see https://github.com/symplely/ext-uv/runs/2779157000?check_suite_focus=true

--- php_uv.c
+++ php_uv.c
@@ -424,7 +424,7 @@ static php_socket_t php_uv_zval_to_valid_poll_fd(zval *ptr)
 
 	/* Validate Checks */
 
-#if !defined(PHP_WIN32) || (defined(HAVE_SOCKETS) && !defined(COMPILE_DL_SOCKETS))
+#if !defined(PHP_WIN32) || (defined(HAVE_SOCKETS) && !defined(COMPILE_DL_SOCKETS)) || PHP_VERSION_ID >= 80000
 	php_socket *socket;
 #endif
 	/* TODO: is this correct on windows platform? */
@@ -474,7 +474,7 @@ static php_socket_t php_uv_zval_to_fd(zval *ptr)
 {
 	php_socket_t fd = -1;
 	php_stream *stream;
-#if !defined(PHP_WIN32) || (defined(HAVE_SOCKETS) && !defined(COMPILE_DL_SOCKETS))
+#if !defined(PHP_WIN32) || (defined(HAVE_SOCKETS) && !defined(COMPILE_DL_SOCKETS)) || PHP_VERSION_ID >= 80000
 	php_socket *socket;
 #endif
 	/* TODO: is this correct on windows platform? */
@@ -798,7 +798,7 @@ static void php_uv_fs_common(uv_fs_type fs_type, INTERNAL_FUNCTION_PARAMETERS)
 		Z_PARAM_FUNC_EX(fci, fcc, 1, 0) \
 	ZEND_PARSE_PARAMETERS_END()
 
-#define PHP_UV_FS_PARSE_PARAMETERS(num, params) PHP_UV_FS_PARSE_PARAMETERS_EX(num, params, 0)	
+#define PHP_UV_FS_PARSE_PARAMETERS(num, params) PHP_UV_FS_PARSE_PARAMETERS_EX(num, params, 0)
 
 #define PHP_UV_FS_SETUP() \
 	PHP_UV_INIT_UV(uv, uv_fs_ce); \
@@ -1231,7 +1231,7 @@ void static destruct_uv_lock(zend_object *obj)
 	}
 }
 
-static void destruct_uv_loop_walk_cb(uv_handle_t* handle, void* arg) 
+static void destruct_uv_loop_walk_cb(uv_handle_t* handle, void* arg)
 {
 	php_uv_t *uv = (php_uv_t *) handle->data;
 	if (!PHP_UV_IS_DTORED(uv)) { // otherwise we're already closing
@@ -2635,7 +2635,7 @@ static zend_object *php_uv_create_uv_loop(zend_class_entry *ce) {
 	php_uv_loop_t *loop = emalloc(sizeof(php_uv_loop_t));
 	zend_object_std_init(&loop->std, ce);
 	loop->std.handlers = &uv_loop_handlers;
-	
+
 	uv_loop_init(&loop->loop);
 
 	loop->gc_buffer_size = 0;
@@ -3122,7 +3122,7 @@ ZEND_BEGIN_ARG_INFO_EX(arginfo_uv_spawn, 0, 0, 7)
 	ZEND_ARG_INFO(0, env)
 	ZEND_ARG_INFO(0, callback)
 	ZEND_ARG_INFO(0, flags)
-	ZEND_ARG_INFO(0, options)	
+	ZEND_ARG_INFO(0, options)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_uv_kill, 0, 0, 2)
@@ -4838,7 +4838,7 @@ PHP_FUNCTION(uv_stdio_new)
 	php_uv_stdio_t *stdio;
 	zval *handle;
 	zend_long flags = 0;
-#if !defined(PHP_WIN32) || defined(HAVE_SOCKET)
+#if !defined(PHP_WIN32) || defined(HAVE_SOCKET) || PHP_VERSION_ID >= 80000
 	php_socket *socket;
 #endif
 	php_socket_t fd = -1;
@@ -5838,7 +5838,7 @@ PHP_FUNCTION(uv_fs_fchown)
 	php_uv_fs_common(UV_FS_FCHOWN, INTERNAL_FUNCTION_PARAM_PASSTHRU);
 }
 /* }}} */
-	
+
 /* {{{ proto void uv_fs_link(UVLoop $loop, string $from, string $to[, callable(long $result) $callback])
 */
 PHP_FUNCTION(uv_fs_link)
